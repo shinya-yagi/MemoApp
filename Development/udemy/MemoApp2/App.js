80b4e30d4b89469bf005df5dd2fafa5c434a8b8f
shinya-yagi <shinya_puredrive@yahoo.co.jp>
2020-03-01 21:49:11 +0900

    Create MemoListScreen

--- Development/udemy/MemoApp2/App.js
+++ Development/udemy/MemoApp2/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import Appbar from './src/components/Appbar';
-import MemoList from './src/components/MemoList';
-import CircleButton from './src/elements/CircleButton';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Appbar />
-      <MemoList />
-      <CircleButton>+</CircleButton>
-    </View>
-  );
+import Appbar from './src/components/Appbar';
+import MemoListScreen from './src/screens/MemoListScreen';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Appbar />
+        <MemoListScreen />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
