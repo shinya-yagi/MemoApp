c697f06457f9edf9e595fa8dcff7e0b1dcb21c23
shinya-yagi <shinya_puredrive@yahoo.co.jp>
2020-03-05 23:53:23 +0900

    Create Login and Signup screen

--- Development/udemy/MemoApp2/App.js
+++ Development/udemy/MemoApp2/App.js
@@ -4,14 +4,16 @@ import { StyleSheet, View } from 'react-native';
 import Appbar from './src/components/Appbar';
 // import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
-import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
+// import LoginScreen from './src/screens/LoginScreen';
+import SignupScreen from './src/screens/SignupScreen';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Appbar />
-        <MemoEditScreen />
+        <SignupScreen />
       </View>
     );
   }
