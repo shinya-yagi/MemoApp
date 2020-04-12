7f17d7a41bd4d66e91a75e9537807d79dd62bbad
shinya-yagi <shinya_puredrive@yahoo.co.jp>
2020-04-13 00:43:55 +0900

    get memolist from firestore

--- Development/udemy/MemoApp2/src/screens/MemoCreateScreen.js
+++ Development/udemy/MemoApp2/src/screens/MemoCreateScreen.js
@@ -9,11 +9,11 @@ class MemoCreateScreen extends React.Component {
   }
 
   handlePress() {
-    const { params } = this.props.navigation.state;
-    console.log(params);
     const db = firebase.firestore();
-    // db.settings({ timestampsInSnapshots: true });
-    db.collection(`users/${params.currentUser.user.uid}/memos`).add({
+    const { currentUser } = firebase.auth();
+    console.log(currentUser);
+    // db.collection(`users/${params.currentUser.user.uid}/memos`).add({
+    db.collection(`users/${currentUser.uid}/memos`).add({
       body: this.state.body,
       createdOn: new Date(),
     })
--- Development/udemy/MemoApp2/src/screens/MemoListScreen.js
+++ Development/udemy/MemoApp2/src/screens/MemoListScreen.js
@@ -6,15 +6,36 @@ import MemoList from '../components/MemoList';
 import CircleButton from '../elements/CircleButton';
 
 class MemoListScreen extends React.Component {
+  state = {
+    memoList: [],
+  }
+
+  componentDidMount() {
+    const { currentUser } = firebase.auth();
+    const db = firebase.firestore();
+    db.collection(`users/${currentUser.uid}/memos`)
+      .get()
+      .then((querySnapshot) => {
+        const tempList = [];
+        querySnapshot.forEach((doc) => {
+          tempList.push(doc.data());
+        });
+        this.setState({ memoList: tempList });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   handlePress() {
     const { params } = this.props.navigation.state;
-    this.props.navigation.navigate('MemoCreate', { currentUser: params.currentUser });
+    this.props.navigation.navigate('MemoCreate');
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <MemoList navigation={this.props.navigation} />
+        <MemoList memoList={this.state.memoList} navigation={this.props.navigation} />
         <CircleButton name="plus" onPress={this.handlePress.bind(this)} />
       </View>
     );
