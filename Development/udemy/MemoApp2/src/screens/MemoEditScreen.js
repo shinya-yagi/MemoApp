4f3f9fafa4a9714c1109c24ed63366f04746e6ab
shinya-yagi <shinya_puredrive@yahoo.co.jp>
2020-04-13 23:39:11 +0900

    memoedit createdon

--- Development/udemy/MemoApp2/src/screens/MemoEditScreen.js
+++ Development/udemy/MemoApp2/src/screens/MemoEditScreen.js
@@ -4,10 +4,19 @@ import { StyleSheet, View, TextInput } from 'react-native';
 import CircleButton from '../elements/CircleButton';
 
 class MemoEditScreen extends React.Component {
-  render() {
+  state = {
+    memo: {},
+  }
+
+  componentDidMount() {
+    const { params } = this.props.navigation.state;
+    this.setState({ memo: params.memo });
+  }
+
+render() {
     return (
       <View style={styles.container}>
-        <TextInput style={styles.memoEditInput} multiline value="hi" />
+        <TextInput style={styles.memoEditInput} multiline value="hi" value={this.state.memo.body} />
         <CircleButton name="check" onPress={() => { this.props.navigation.goBack(); }} />
 
       </View>
