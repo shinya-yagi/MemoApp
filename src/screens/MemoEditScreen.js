c40e8b3926b46571651adda178d47bc134703ff2
shinya.yagi <shinya_puredrive@yahoo.co.jp>
2020-04-19 00:37:24 +0900

    expo firebase init

---
+++ src/screens/MemoEditScreen.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { StyleSheet, View, TextInput } from 'react-native';
+import firebase from 'firebase';
+import CircleButton from '../elements/CircleButton';
+
+class MemoEditScreen extends React.Component {
+  state = {
+    body: '',
+    key: '',
+  }
+
+  componentDidMount() {
+    const { params } = this.props.navigation.state;
+    this.setState({
+      body: params.memo.body,
+      key: params.memo.key
+    });
+  }
+
+  handlePress() {
+    const { currentUser } = firebase.auth();
+    const db = firebase.firestore();
+    console.log(this.state);
+    db.collection(`users/${currentUser.uid}/memos`).doc(this.state.key)
+      .update({
+        body: this.state.body,
+      })
+      .then(() => {
+        console.log('success');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.memoEditInput}
+          multiline
+          value="hi"
+          value={this.state.body}
+          onChangeText={(text) => { this.setState({ body: text }); }}
+        />
+        <CircleButton name="check" onPress={this.handlePress.bind(this)} />
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+  },
+  memoEditInput: {
+    backgroundColor: '#fff',
+    flex: 1,
+    paddingTop: 32,
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingBottom: 16,
+    fontSize: 16,
+  },
+});
+
+export default MemoEditScreen;
